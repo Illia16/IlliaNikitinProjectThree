4d2d82ff7bfecca5c626a3f0be78fb4bcc362d2f
Illia <illia2143@gmail.com>
2020-07-22 22:31:18 -0400

    major portion of logic is done + css/html started. :+1::musical_note:

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 const mealApp = {};
 
-mealApp.chosenByUser = [];
+mealApp.chosenIngridients = [];
 
-mealApp.cookedMeals = {};
+mealApp.cookedMeals = [];
 
 mealApp.answers = {
     smoothie: ['banana', 'milk', 'pineapple'],
@@ -17,9 +17,34 @@ mealApp.availableIngridients = function() {
     for (i=0; i < mealApp.allIngridients.length; i++) {
         $('.ingridients').append(`
         <li>
-            <button id="remove">Remove</button>
-            <p>${mealApp.allIngridients[i]}</p>
-            <button id="add">Add</button>
+            <h3>${mealApp.allIngridients[i]}</h3>
+            <div><img src="./assets/${mealApp.allIngridients[i]}.png" alt="${mealApp.allIngridients[i]}"></img></div>
+            <button id="add" value="${mealApp.allIngridients[i]}">Add</button>
+            <button id="remove" value="${mealApp.allIngridients[i]}">Remove</button>
+        </li>
+        `);
+    }
+
+    for (const meals in  mealApp.answers) {
+        // const everyMeal = meals;
+        $('.cookedMeals').append(`
+        <li class="${meals}">
+            <h3>${meals}</h3>
+            <div><img src="./assets/meals/${meals}.png" alt="${meals}"></img></div>
+            <p>Ingridients needed to cook: ${mealApp.answers[meals]}</p>
+        </li>
+        `);
+    }
+}
+
+// Display dinamically on the table chosen ingridients 
+mealApp.displayChosen = () => {
+    $('.chosenIngridients').text('');
+    for (i=0; i < mealApp.chosenIngridients.length; i++) {
+        $('.chosenIngridients').append(`
+        <li>
+            <h3>${mealApp.chosenIngridients[i]}</h3>
+            <div><img src="./assets/${mealApp.chosenIngridients[i]}.png" alt="${mealApp.chosenIngridients[i]}"></img></div>
         </li>
         `);
     }
@@ -31,48 +56,82 @@ mealApp.checkIfCooked = function() {
     for (const meals in  mealApp.answers) {
         // console.log(`${meals}: ${mealApp.answers[meals]}`);
         
-        if (mealApp.chosenByUser.sort().join(',') === mealApp.answers[meals].sort().join(',')){
+        if (mealApp.chosenIngridients.sort().join(',') === mealApp.answers[meals].sort().join(','))
+        {
+            mealApp.cookedMeals.push(meals);
+            mealApp.chosenIngridients = [];
+            console.log("Cooked meals:", mealApp.cookedMeals);
+
+            $(`.cookedMeals .${meals}`).toggleClass('unlocked'); ////////
             return `We made a ${meals}`;
         }
-        // else {
-        //     continue
-        // }
     }
-    return `Nothing's cooked yet!`
+    return `Nothing's cooked yet!`;
 }
 
 
 // Adding ingridient we want
 $('ul').on('click', '#add', function(){
-    const pickedIngridient = $(this).prev().text();
-    mealApp.chosenByUser.push(pickedIngridient);
-    console.log(mealApp.chosenByUser);
+    const pickedIngridient = $(this).val();
+    mealApp.chosenIngridients.push(pickedIngridient);
+    console.log(mealApp.chosenIngridients);
 
     const result = mealApp.checkIfCooked();
     console.log(result);
+
+    mealApp.displayChosen();
 });
 
 // Removing ingridient we want
 $('ul').on('click', '#remove', function(){
-    const toRemove = $(this).next().text();
-    console.log(toRemove);
+    const toRemove = $(this).val();
+    console.log("What to remove:", toRemove);
 
-    const toRemoveIndex = mealApp.chosenByUser.indexOf(toRemove);
-    console.log(toRemoveIndex);
+    const toRemoveIndex = mealApp.chosenIngridients.indexOf(toRemove);
+    console.log("Index of the item to be removed:", toRemoveIndex);
 
-    mealApp.chosenByUser.splice(toRemoveIndex, 1);
-    console.log(mealApp.chosenByUser);
+    if (mealApp.chosenIngridients.length === 0) {
+        console.log('Nothing\'s on the table!');
+    } else if (toRemoveIndex === -1) {
+        console.log(`${toRemove} is not on the table!`);
+    } else mealApp.chosenIngridients.splice(toRemoveIndex, 1);
+    console.log("after removal:", mealApp.chosenIngridients);
 
     const result = mealApp.checkIfCooked();
     console.log(result);
+
+    mealApp.displayChosen();
 });
 
 
 
 
+$('.cooked').on('click', function(event){
+    event.preventDefault();
+    $('.cookedMeals').toggleClass('cookedMealsShow');
+});
+
+// mealApp.startOver = function() {
+    $('#toMain').on('click', function(event){
+        event.preventDefault();
+        mealApp.scroll('section');
+    });
+//}
+
+
+//smooth scrool to come to a desired part of the page
+mealApp.scroll = function(element) {
+	$('html').animate(
+		{
+            scrollTop: $(element).offset().top
+		}, 1000
+	);
+};
+
 
 mealApp.init = function() {
     mealApp.availableIngridients();
+    // mealApp.startOver();
 }
 
 //Document ready
